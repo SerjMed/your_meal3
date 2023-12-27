644a2fcc0485e7a3cafc15cfef3942070a67a62d
SerjMed <154935877+SerjMed@users.noreply.github.com>
2023-12-27 17:42:55 +0300

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:3024';
+export const API_URL = 'https://jeweled-tender-giganotosaurus.glitch.me';
 export const PREFIX_PRODUCT = '/api/product';
 //export используется для того чтобы const использовать в других файлах
